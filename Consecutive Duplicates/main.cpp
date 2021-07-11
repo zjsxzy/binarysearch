752059e5e0b483cfc825ecc125f30004ed1d3063
zjsxzy <zjsxzy@gmail.com>
2021-07-11 17:45:21 +0800

    consecutive duplicates

---
+++ Consecutive Duplicates/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+string solve(string s) {
+    string res;
+    char prev;
+    for (auto &c: s) {
+        if (c != prev) {
+            res += c;
+        }
+        prev = c;
+    }
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+

e79f062accfc9f3da0910dad45d77e09095fe852
Zhang Yang <zjsxzy@gmail.com>
2021-07-23 13:19:01 +0800

    line of people

---
+++ Line of people/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+int solve(int n, int a, int b) {
+    int res = n - a;
+    res = min(res, b + 1);
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+

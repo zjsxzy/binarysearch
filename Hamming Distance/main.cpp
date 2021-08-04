45df776ac727af6eec206cbe0b17773061b53a5d
Zhang Yang <zjsxzy@gmail.com>
2021-08-04 15:04:48 +0800

    hamming distance

---
+++ Hamming Distance/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+int solve(int x, int y) {
+    int z = x ^ y;
+    int res = 0;
+    while (z) {
+        res += (z & 1);
+        z >>= 1;
+    }
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+

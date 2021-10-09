6e4ca5fa55eeaaefbe145d2db6d6284cb8314854
zjsxzy <zjsxzy@gmail.com>
2021-10-09 18:17:18 +0800

    check palindrome

---
+++ Check Palindrome/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+bool solve(string s) {
+    int n = s.size();
+    for (int i = 0; i < n / 2; i++) {
+        if (s[i] != s[n - 1 - i]) return false;
+    }
+    return true;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+

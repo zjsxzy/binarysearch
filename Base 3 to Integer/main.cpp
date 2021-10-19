a0cac3be33e345bdca9caeda0f34df53fba24443
zjsxzy <zjsxzy@gmail.com>
2021-10-19 21:19:46 +0800

    base 3 to integer

---
+++ Base 3 to Integer/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+int solve(string s) {
+    reverse(s.begin(), s.end());
+    int res = 0, d = 1;
+    for (auto c: s) {
+        res += d * (c - '0');
+        d *= 3;
+    }
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+

7241d3213de5680b99afb0b13e1fe42f98498350
Zhang Yang <zjsxzy@gmail.com>
2021-10-22 14:54:01 +0800

    max product of two numbers

---
+++ Longest Alliteration/Max Product of Two Numbers/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+int solve(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    int res = max(nums[n - 1] * nums[n - 2], nums[0] * nums[1]);
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+

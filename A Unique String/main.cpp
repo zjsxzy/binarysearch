ed7f0bcad9848347b7d8a2dcf6fb8ba8f7730841
Zhang Yang <zjsxzy@gmail.com>
2021-12-08 15:20:37 +0800

    a unique string

---
+++ A Unique String/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+bool solve(string s) {
+    set<int> st;
+    for (auto c: s) {
+        if (st.find(c) != st.end()) return false;
+        st.insert(c);
+    }
+    return true;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+

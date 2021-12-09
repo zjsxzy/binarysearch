27ea02e5af4e5345eb69b429e2a52e8823acfa69
Zhang Yang <zjsxzy@gmail.com>
2021-12-09 10:28:04 +0800

    sum of the digits

---
+++ Sum of the Digits/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+int solve(int num) {
+    int res = 0;
+    while (num) {
+        res += num % 10;
+        num /= 10;
+    }
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+

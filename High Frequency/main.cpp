db324d7ea1e018e46d0ebe537a46be7685609269
Zhang Yang <zjsxzy@gmail.com>
2021-12-29 16:10:31 +0800

    high frequency

---
+++ High Frequency/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+int solve(vector<int>& nums) {
+    map<int, int> cnt;
+    for (auto x: nums) {
+        cnt[x]++;
+    }
+    int res = 0;
+    for (auto [k, v]: cnt) {
+        res = max(res, v);
+    }
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+

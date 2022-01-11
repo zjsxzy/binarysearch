17dbc338ac9bc3d93665bdea15d3b2f7012650a5
Zhang Yang <zjsxzy@gmail.com>
2022-01-11 10:05:31 +0800

    prime factorization

---
+++ Prime Factorization/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+vector<int> solve(int n) {
+    vector<int> res;
+    while (n % 2 == 0) {
+        res.push_back(2);
+        n /= 2;
+    }
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            res.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 2) res.push_back(n);
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+

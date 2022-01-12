7051fe214efb2247c31a919b4a0d5a6de7de528d
Zhang Yang <zjsxzy@gmail.com>
2022-01-12 10:50:49 +0800

    sum of first n odd integers

---
+++ Sum of First N Odd Integers/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+int solve(int n) {
+    int res = 0;
+    for (int i = 1, j = 0; j < n; i += 2, j++) {
+        res += i;
+    }
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+

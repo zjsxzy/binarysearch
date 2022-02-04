638258af5f087073631149ad3eab9744492212db
zjsxzy <zjsxzy@gmail.com>
2022-02-04 20:03:07 +0800

    coincidence search

---
+++ Coincidence Search/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+int solve(vector<int>& nums) {
+    int res = 0;
+    for (auto target: nums) {
+        int lo = 0, hi = nums.size() - 1;
+        while (lo <= hi) {
+            int mid = (lo + hi) >> 1;
+            if (nums[mid] == target) {
+                res++;
+                break;
+            } else if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+    }
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+

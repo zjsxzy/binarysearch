226bc640ffab0daa10cc6a4ce4da788c3b5be43f
Zhang Yang <zjsxzy@gmail.com>
2022-03-16 10:23:57 +0800

    flip to zeros

---
+++ Flip to Zeros/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+int solve(vector<int>& nums) {
+    int res = 0, now = 1;
+    for (auto x: nums) {
+        if (x == now) {
+            res++;
+            now = 1 - now;
+        }
+    }
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    solve();
+    return 0;
+}
+

ad17cfd9ffd639bb5b3b8f402125d4a7ad121168
Zhang Yang <zjsxzy@gmail.com>
2022-07-19 10:52:34 +0800

    enclosed islands

---
+++ Enclosed Islands/main.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+struct union_find{
+    int N;
+    vector<int> par, siz;
+    union_find(int n) : N(n){
+        par.resize(N);
+        siz.resize(N, 1);
+        for(int i=0; i<N; i++) par[i] = i;
+    }
+    int root(int X){
+        if(par[X] == X) return X;
+        return par[X] = root(par[X]);
+    }
+    bool same(int X, int Y){
+        return root(X) == root(Y);
+    }
+    void unite(int X, int Y){
+        X = root(X);
+        Y = root(Y);
+        if(X == Y) return;
+        if(siz[Y] < siz[X]) std::swap(X, Y);
+        par[X] = Y;
+        siz[Y] += siz[X];
+        siz[X] = 0;
+    }
+};
+
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+
+int solve(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    if (n == 0) return 0;
+    int m = matrix[0].size();
+    if (m == 0) return 0;
+    union_find uf(n * m + 1);
+    int source = n * m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 1) {
+                if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
+                    uf.unite(i * m + j, source);
+                }
+                for (int k = 0; k < 4; k++) {
+                    int x = i + dx[k], y = j + dy[k];
+                    if (x >= 0 && x < n && y >= 0 && y < m && matrix[x][y] == 1) {
+                        uf.unite(i * m + j, x * m + y);
+                    }
+                }
+            }
+        }
+    }
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 1 && !uf.same(i * m + j, source)) res++;
+        }
+    }
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+   (void)!freopen("in.txt", "r", stdin);
+#endif
+   int ts = 1;
+   // cin >> ts;
+   for (int t = 1; t <= ts; t++) {
+       solve();
+   }
+   return 0;
+}
+

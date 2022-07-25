857843301b7fec626c3324ddad8cf67932a8076c
Zhang Yang <zjsxzy@gmail.com>
2022-07-25 10:32:52 +0800

    maximum non-adjacent tree sum

---
+++ Maximum Non-Adjacent Tree Sum/main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+/**
+ * class Tree {
+ *     public:
+ *         int val;
+ *         Tree *left;
+ *         Tree *right;
+ * };
+ */
+int solve(Tree* root) {
+    unordered_map<Tree*, int> f, g;
+    function<void(Tree*)> dfs = [&](Tree* node) {
+        if (!node) return;
+        dfs(node->left);
+        dfs(node->right);
+        f[node] = node->val + g[node->left] + g[node->right];
+        g[node] = max(f[node->left], g[node->left]) + max(f[node->right], g[node->right]);
+    };
+    dfs(root);
+    return max(f[root], g[root]);
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+   (void)!freopen("in.txt", "r", stdin);
+#endif
+   int ts = 1;
+   // cin >> ts;
+   for (int t = 1; t <= ts; t++) {
+       solve();
+   }
+   return 0;
+}
+

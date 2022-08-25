df4db6923853f335b0c36fa66c2fb7631126f61f
Zhang Yang <zjsxzy@gmail.com>
2022-08-25 10:04:34 +0800

    group points

---
+++ Group Points/main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+struct union_find{
+    int N;
+    vector<int> par, siz;
+    union_find(int n) : N(n){
+        par.resize(N);
+        siz.resize(N, 1);
+        for(int i=0; i<N; i++) par[i] = i;
+    }
+    int root(int X){
+        if(par[X] == X) return X;
+        return par[X] = root(par[X]);
+    }
+    bool same(int X, int Y){
+        return root(X) == root(Y);
+    }
+    void unite(int X, int Y){
+        X = root(X);
+        Y = root(Y);
+        if(X == Y) return;
+        if(siz[Y] < siz[X]) std::swap(X, Y);
+        par[X] = Y;
+        siz[Y] += siz[X];
+        siz[X] = 0;
+    }
+};
+
+int solve(vector<vector<int>>& points, int k) {
+    int n = points.size();
+    union_find uf(n);
+    auto dist = [&](vector<int>& a, vector<int>& b) {
+        return (a[0] - b[0]) * (a[0] - b[0]) + (a[1] - b[1]) * (a[1] - b[1]);
+    };
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int d2 = dist(points[i], points[j]);
+            if (d2 <= k * k) {
+                uf.unite(i, j);
+            }
+        }
+    }
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        if (i == uf.root(i)) res++;
+    }
+    return res;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+   (void)!freopen("in.txt", "r", stdin);
+#endif
+   int ts = 1;
+   // cin >> ts;
+   for (int t = 1; t <= ts; t++) {
+       solve();
+   }
+   return 0;
+}
+
